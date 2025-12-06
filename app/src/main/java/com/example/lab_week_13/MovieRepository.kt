fa3fba7eaf2e22071ca7804e9ea2f4ad83a28742
Jimagination <jimmytandra65@gmail.com>
2025-12-06 16:48:20 +0700

    Commit No. 1 - add data binding

---
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -0,0 +1,19 @@
+package com.example.lab_week_13
+
+import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+
+class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "b6af965b45f85396e37bb2a6ddb3e925"
+
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
+    }
+}
